2a33c92ad6ac92fce54be6205cc1fc95ddb3c50e
Asmitumrao <ashmitumrao@gmail.com>
2025-05-11 04:55:33 +0530

    fixed cors error

--- backend/nodeBackend/src/index.js
+++ backend/nodeBackend/src/index.js
@@ -14,14 +14,15 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors(
-  {
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  }
-));
+app.use(cors({ origin: ['https://heart-byte.vercel.app', 'http://localhost:5173'],credentials: true }));
+// // Middleware
+// app.use(cors(
+//   {
+//     origin: process.env.CLIENT_URL || 'http://localhost:5173',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   }
+// ));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
