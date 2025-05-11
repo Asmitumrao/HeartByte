125e048ddae86e58002360aad82b0eeb1f060a4b
Anisha <anishaworks21@gmail.com>
2025-05-11 17:51:34 +0530

    Link to diet

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,11 +1,14 @@
-import { FaUserMd, FaStethoscope, FaFileAlt, FaCog, FaClipboardList,FaQuestionCircle } from 'react-icons/fa';
+import { FaUserMd, FaStethoscope, FaFileAlt, FaCog, FaClipboardList, FaQuestionCircle } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.png';
 export default function Sidebar({ selected, onSelect }) {
   const navigate = useNavigate();
   return (
     <div className="bg-white w-64 min-h-screen shadow-md flex flex-col justify-between">
       <div>
         <div className="p-6 border-b">
+          <img src={logo} />
           <h1 className="text-lg font-semibold text-gray-700">Doctor Dashboard 🩺</h1>
         </div>
         <ul className="mt-4">
@@ -19,14 +22,18 @@ export default function Sidebar({ selected, onSelect }) {
             className={`pl-6 py-3 cursor-pointer hover:bg-gray-100 ${selected === 'patients' ? 'border-l-4 border-blue-500 bg-gray-100' : ''}`}
             onClick={() => onSelect('patients')}
           >
+      
             <FaFileAlt className="inline mr-3 text-blue-500" /> Patients
-          </li>
-                    <li
-            className={`pl-6 py-3 cursor-pointer hover:bg-gray-100 ${selected === 'diet' ? 'border-l-4 border-blue-500 bg-gray-100' : ''}`}
-            onClick={() => onSelect('diet')}
-          >
-            <FaClipboardList className="inline mr-3 text-blue-500" /> Diet Plan
-          </li>
+            </li>
+            <li>
+              <Link
+                to="/dietplan"
+                className={`block pl-6 py-3 cursor-pointer hover:bg-gray-100 ${selected === 'diet' ? 'border-l-4 border-blue-500 bg-gray-100' : ''}`}
+                onClick={() => onSelect('diet')}
+              >
+                <FaClipboardList className="inline mr-3 text-blue-500" /> Diet Plan
+              </Link>
+            </li>
         </ul>
       </div>
       <div className="mb-6 px-6">
@@ -37,7 +44,7 @@ export default function Sidebar({ selected, onSelect }) {
             <p className="text-xs">Doctor</p>
           </div>
           <div>
-            <button className='bg-blue-600 text-white px-3 py-2 rounded-lg' onClick={()=>{navigate('/')}}>Back</button>
+            <button className='bg-blue-600 text-white px-3 py-2 rounded-lg' onClick={() => { navigate('/') }}>Back</button>
           </div>
         </div>
       </div>
